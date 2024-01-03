e215166371e5a1f29311022adacd2e584dcffe97
Gustavo Braga <gb95131aq@gmail.com>
2024-01-03 13:20:08 -0300

    Upload Project

---
+++ AnyDesk_Remove_Timer/Program.cs
@@ -0,0 +1,25 @@
+﻿string path = Environment.ExpandEnvironmentVariables(@"%AppData%\AnyDesk");
+bool existeDiretorio = Directory.Exists(path);
+
+if (existeDiretorio)
+{
+    try
+    {
+        Directory.Delete(path, true);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"[{DateTime.Now}] Anydesk resetado");
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"[{DateTime.Now}] Erro: {ex.Message}");
+    }
+
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"[{DateTime.Now}] Anydesk não encontrado, abra o AnyDesk, feche e tente novamente");
+}
+
+Console.ReadKey();
